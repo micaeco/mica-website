8078549cbc3d753a4c8a56a9390db8807d884111
micaeco <admin@mica.eco>
2024-08-01 12:23:46 +0200

    Included vercel analytics component on the root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from '@vercel/analytics/react';
 import { Montserrat } from "next/font/google";
 import "./globals.css";
 
@@ -23,6 +24,7 @@ export default function RootLayout({
         <Header />
         {children}
         <Footer />
+        <Analytics />
       </body>
     </html>
   );
