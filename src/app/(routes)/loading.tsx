42dcb65a81f5d1aec1de6ea50ef1368a71cb8e6e
micaeco <admin@mica.eco>
2024-08-30 01:06:10 +0200

    Minor changes

--- src/app/(routes)/loading.tsx
+++ src/app/(routes)/loading.tsx
@@ -1,9 +1,5 @@
-import React from 'react';
+import Loading from '@/src/components/sections/common/Loading';
 
-export default function Loading() {
-  return (
-    <div className="flex h-screen items-center justify-center">
-      <div className="h-32 w-32 animate-spin rounded-full border-b-2 border-gray-900"></div>
-    </div>
-  );
+export default function RootLoading() {
+  return <Loading />;
 }
