07e20b2acda7fd77f10dae72a61d357e70f6d517
micaeco <admin@mica.eco>
2024-08-30 15:34:13 +0200

    Minor changes

--- src/constants/palette.ts
+++
@@ -1,8 +0,0 @@
-export const colors = {
-  primary: '220 100% 50%',
-  secondary: '180 100% 50%',
-  tertiary: '120 100% 50%',
-  accent: '300 100% 50%',
-};
-
-export type colorName = keyof typeof colors;
