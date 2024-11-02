7306844520f5ab2905b126565a9912e9912f4e05
micaeco <admin@mica.eco>
2024-11-02 11:21:20 +0100

    Minor changes

--- src/app/[locale]/(routes)/components/hero.tsx
+++ src/app/[locale]/(routes)/components/hero.tsx
@@ -20,7 +20,7 @@ export default function Hero() {
   return (
     <section className="flex flex-col items-center bg-white px-8 pb-4 pt-20">
       <div className="relative flex w-full max-w-6xl flex-col gap-16 text-left 2xl:h-[650px]">
-        <div className="absolute right-5 top-4 -z-10 hidden items-center 2xl:block">
+        <div className="absolute right-5 top-4 hidden items-center 2xl:block">
           <Image
             src="/images/hero-graphic-desktop.webp"
             alt="Background"
@@ -28,7 +28,7 @@ export default function Hero() {
             height={720}
           />
         </div>
-        <div className="mx-auto justify-center space-y-4 text-left 2xl:mx-0 2xl:justify-start">
+        <div className="z-5 mx-auto justify-center space-y-4 text-left 2xl:mx-0 2xl:justify-start">
           <Image
             src="/images/hero-graphic-mobile.webp"
             alt="Logo"
