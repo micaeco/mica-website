5fbd2e251bc59c8908fb4d005410b37eae1373a2
micaeco <admin@mica.eco>
2024-11-02 11:24:36 +0100

    Minor changes

--- src/app/[locale]/(routes)/components/hero.tsx
+++ src/app/[locale]/(routes)/components/hero.tsx
@@ -20,7 +20,7 @@ export default function Hero() {
   return (
     <section className="flex flex-col items-center bg-white px-8 pb-4 pt-20">
       <div className="relative flex w-full max-w-6xl flex-col gap-16 text-left 2xl:h-[650px]">
-        <div className="absolute right-5 top-4 hidden items-center 2xl:block">
+        <div className="pointer-events-none absolute right-5 top-4 hidden 2xl:block">
           <Image
             src="/images/hero-graphic-desktop.webp"
             alt="Background"
@@ -28,7 +28,7 @@ export default function Hero() {
             height={720}
           />
         </div>
-        <div className="z-5 mx-auto justify-center space-y-4 text-left 2xl:mx-0 2xl:justify-start">
+        <div className="z-10 mx-auto justify-center space-y-4 text-left 2xl:mx-0 2xl:justify-start">
           <Image
             src="/images/hero-graphic-mobile.webp"
             alt="Logo"
@@ -36,21 +36,23 @@ export default function Hero() {
             height={300}
             className="block 2xl:hidden"
           />
-          <h1 className="max-w-[500px] text-pretty font-bold leading-tight">{t('title')}</h1>
-          <p className="max-w-[400px] text-pretty font-light">{t('text')}</p>
-          <Link href="beta">
-            <Button
-              className="mb-4 mt-10 2xl:mb-24"
-              variant="expandIcon"
-              Icon={ArrowRightIcon}
-              iconPlacement="right"
-              size="lg"
-            >
-              {t('cta')}
-            </Button>
-          </Link>
-          <p className="text-sm font-light">{t('with-the-support')}</p>
-          <Image src={'/logos/bithabitat.png'} alt={'Logo bithabitat'} width={160} height={160} />
+          <div className="relative">
+            <h1 className="max-w-[500px] text-pretty font-bold leading-tight">{t('title')}</h1>
+            <p className="max-w-[400px] text-pretty font-light">{t('text')}</p>
+            <Link href="beta">
+              <Button
+                className="mb-4 mt-10 2xl:mb-24"
+                variant="expandIcon"
+                Icon={ArrowRightIcon}
+                iconPlacement="right"
+                size="lg"
+              >
+                {t('cta')}
+              </Button>
+            </Link>
+            <p className="text-sm font-light">{t('with-the-support')}</p>
+            <Image src={'/logos/bithabitat.png'} alt={'Logo bithabitat'} width={160} height={160} />
+          </div>
         </div>
       </div>
       <div className="flex justify-center py-16">
