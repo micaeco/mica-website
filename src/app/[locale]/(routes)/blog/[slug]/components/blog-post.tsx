1ebc40ceb6ae8e61d3dc70a5682ed370476f3821
micaeco <admin@mica.eco>
2024-11-02 11:43:17 +0100

    Minor changes

--- src/app/[locale]/(routes)/blog/[slug]/components/blog-post.tsx
+++ src/app/[locale]/(routes)/blog/[slug]/components/blog-post.tsx
@@ -1,18 +1,65 @@
-import { Clock, User, Tag, Book } from 'lucide-react';
-import { useTranslations } from 'next-intl';
+'use client';
 
+import { useState, useEffect } from 'react';
+import { useTranslations } from 'next-intl';
+import { Clock, User, Tag, Book } from 'lucide-react';
+
+import Loading from '@/components/loading';
 import MarkdownRenderer from '@/components/ui/markdown';
 import GoBack from '@/components/ui/go-back';
 import { IPost } from '@/types';
 import { languageMap } from '@/lib/constants';
 
 type Props = {
-  post: IPost;
+  params: {
+    slug: string;
+  };
 };
 
-export default function Post({ post }: Props) {
+export default function BlogPostPage({ params }: Props) {
+  const { slug } = params;
+  const [posts, setPosts] = useState<IPost[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  const errors = useTranslations('errors');
   const common = useTranslations('common');
   const tTags = useTranslations('blog.tags');
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const response = await fetch('/api/blog-posts');
+        const data = await response.json();
+
+        if (!response.ok) {
+          setError(errors(data.error));
+        }
+
+        const fetchedPosts = data.posts;
+        setPosts(fetchedPosts);
+      } catch (error) {
+        setError(error as string);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchPosts();
+  }, []);
+
+  if (isLoading) return <Loading />;
+  if (error) return <div>{errors(error)}</div>;
+
+  const post = posts.find((post) => post.slug === slug);
+
+  if (!post)
+    return (
+      <div className="flex h-screen flex-col items-center justify-center">
+        {errors('POST_NOT_FOUND')}
+        <GoBack text={common('go-back')} />
+      </div>
+    );
 
   return (
     <article className="mx-auto max-w-3xl px-4 py-8">
--- src/app/[locale]/(routes)/blog/[slug]/page.tsx
+++ src/app/[locale]/(routes)/blog/[slug]/page.tsx
@@ -1,12 +1,4 @@
-'use client';
-
-import { useState, useEffect } from 'react';
-import { useTranslations } from 'next-intl';
-
-import Loading from '@/components/loading';
 import BlogPost from './components/blog-post';
-import { IPost } from '@/types';
-import GoBack from '@/components/ui/go-back';
 
 type Props = {
   params: {
@@ -15,48 +7,5 @@ type Props = {
 };
 
 export default function BlogPostContent({ params }: Props) {
-  const { slug } = params;
-  const [posts, setPosts] = useState<IPost[]>([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState<string | null>(null);
-
-  const errors = useTranslations('errors');
-  const common = useTranslations('common');
-
-  useEffect(() => {
-    const fetchPosts = async () => {
-      try {
-        const response = await fetch('/api/blog-posts');
-
-        const data = await response.json();
-
-        if (!response.ok) {
-          setError(errors(data.error));
-        }
-
-        const fetchedPosts = data.posts;
-        setPosts(fetchedPosts);
-      } catch (error) {
-        setError(error as string);
-      } finally {
-        setIsLoading(false);
-      }
-    };
-
-    fetchPosts();
-  }, []);
-
-  if (isLoading) return <Loading />;
-  if (error) return <div>{errors(error)}</div>;
-
-  const post = posts.find((post) => post.slug === slug);
-
-  if (!post)
-    return (
-      <div className="flex h-screen flex-col items-center justify-center">
-        {errors('POST_NOT_FOUND')}
-        <GoBack text={common('go-back')} />
-      </div>
-    );
-  return <BlogPost post={post} />;
+  return <BlogPost params={params} />;
 }
