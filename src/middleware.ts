2a3d93b0eb2d9d8548ddfe5ae6a56c512d0baf54
micaeco <admin@mica.eco>
2024-11-09 01:22:03 +0100

    Changed middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,6 +4,5 @@ import { routing } from './i18n/routing';
 export default createMiddleware(routing);
 
 export const config = {
-  // Match only internationalized pathnames
-  matcher: ['/', '/(ca|es|en)/:path*']
+  matcher: ['/', '/((?!api|_next|.*\\.|favicon.ico).*)']
 };
